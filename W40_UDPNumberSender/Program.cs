0007e432982046cabb3c8d7953cb41c2773d7831
Lardman <Lardman@DESKTOP-FQBNP2K>
2017-10-06 10:48:00 +0200

    Add project files.

---
+++ W40_UDPNumberSender/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W40_UDPNumberSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
